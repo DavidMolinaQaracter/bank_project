78683f1725ebd59d2309a8cfac903d6074279bb5
Recep Borekci <recep.borekci@qaracter.com>
2026-01-05 14:53:40 +0300

    Correct the package names of the Card project files.

--- src/main/java/entities/DebitCard.java
+++ src/main/java/entities/DebitCard.java
@@ -1,4 +1,4 @@
-package org.example;
+package entities;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
--- src/main/java/entities/Status.java
+++ src/main/java/entities/Status.java
@@ -1,4 +1,4 @@
-package org.example;
+package entities;
 
 public enum Status {
     ACTIVE, CLOSED
